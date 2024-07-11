f6932df7078314c79e3fc3abbae2c4db23794e4a
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-07-11 15:55:04 +0530

    Add zod validations

---
+++ validations/signUpSchema.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+export const usernameSchema = z
+  .string()
+  .min(2, 'Username should be at least 2 characters')
+  .max(15, 'Username should not be more than 20 characters')
+  .regex(/^[a-zA-Z0-9_]+$/, 'Username should not contain special character');
+
+export const singUpSchema = z.object({
+  username: usernameSchema,
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z
+    .string()
+    .min(6, { message: 'password must be at least 6 characters' }),
+});

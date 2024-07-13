f94662cccce63caa70912efc0a35bf618be0af32
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-07-13 16:14:25 +0530

    Add send message route

---
+++ app/api/send-message/route.ts
@@ -0,0 +1,51 @@
+import UserModel from '@/models/User';
+import dbConnect from '@/lib/dbConnect';
+import { Message } from '@/models/User';
+
+export async function POST(request: Request) {
+  await dbConnect();
+  const { username, content } = await request.json();
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: 'User not found',
+        },
+        { status: 404 }
+      );
+    }
+    // is user accepting the messages
+    if (!user.isAcceptingMessage) {
+      return Response.json(
+        {
+          success: false,
+          message: 'User is not accepting message',
+        },
+        { status: 403 }
+      );
+    }
+
+    const newMessage = { content, createdAt: new Date() };
+    user.messages.push(newMessage as Message);
+    await user.save();
+
+    return Response.json(
+      {
+        success: true,
+        message: 'Message sent successfully',
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log('Error adding messages', error);
+    return Response.json(
+      {
+        success: false,
+        message: 'Internal Server Error',
+      },
+      { status: 500 }
+    );
+  }
+}

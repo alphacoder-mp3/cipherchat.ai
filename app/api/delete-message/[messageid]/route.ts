00af6ba0196fb40053faa2c83272a219eed8f83b
alphacoder-mp3 <shindeakshay223@gmail.com>
2024-07-13 16:31:56 +0530

    Update variable names in delete route

--- app/api/delete-message/[messageid]/route.ts
+++ app/api/delete-message/[messageid]/route.ts
@@ -8,7 +8,7 @@ export async function DELETE(
   request: Request,
   { params }: { params: { messageid: string } }
 ) {
-  const messageid = params.messageid;
+  const messageId = params.messageid;
   await dbConnect();
 
   const session = await getServerSession(authOptions);
@@ -25,15 +25,15 @@ export async function DELETE(
   }
 
   try {
-    const updateResult = await UserModel.updateOne(
+    const updatedResult = await UserModel.updateOne(
       { _id: user._id },
-      { $pull: { messages: { _id: messageid } } }
+      { $pull: { messages: { _id: messageId } } }
     );
-    if (updateResult.modifiedCount == 0) {
+    if (updatedResult.modifiedCount == 0) {
       return Response.json(
         {
           success: false,
-          message: 'Message Not found or already delete',
+          message: 'Message Not found or already deleted',
         },
         { status: 404 }
       );
@@ -46,7 +46,7 @@ export async function DELETE(
       { status: 200 }
     );
   } catch (error) {
-    console.log('Error id delete message route', error);
+    console.log('Error in delete message route', error);
     return Response.json(
       {
         success: false,
